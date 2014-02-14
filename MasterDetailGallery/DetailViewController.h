b5592ddfe090ee932fe75b9dca9831c06778e0dc
pony wu <mac@mac.local>
2014-02-14 21:40:35 +0800

    Initial Commit

---
+++ MasterDetailGallery/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  MasterDetailGallery
+//
+//  Created by pony wu on 14-2-14.
+//  Copyright (c) 2014年 pony wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ MasterDetailGallery/MasterViewController.h
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.h
+//  MasterDetailGallery
+//
+//  Created by pony wu on 14-2-14.
+//  Copyright (c) 2014年 pony wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
